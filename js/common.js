bce9a99a13bf29bb5c2679b65cd7d232f69be9dc
nickolay.grin <nick@singularitysolutions.com>
2024-03-29 16:53:17 +0200

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
 
+	/*new*/
+		/*animate*/
+		new WOW().init();
+
+		$(".item-lunch__content").click(function() {
+			var dataTitle = $(this).attr("data-title");
+			$(".form-lunch .btn-main span").html(dataTitle);
+		});
 
 //прилипающие меню
 var $menu = $(".header");
@@ -156,6 +164,37 @@ if ( $(this).scrollTop() > 0 && $menu.hasClass("default") ){
 		initialCountry:"ru",
 		utilsScript: "libs/intl-tel-input-master/build/js/utils.js",
 
+	});
+
+	jQuery('.quantity').each(function() {
+		var spinner = jQuery(this),
+		input = spinner.find('input[type="number"]'),
+		btnUp = spinner.find('.quantity-up'),
+		btnDown = spinner.find('.quantity-down'),
+		min = input.attr('min'),
+		max = input.attr('max');
+
+		btnUp.click(function() {
+			var oldValue = parseFloat(input.val());
+			if (oldValue >= max) {
+				var newVal = oldValue;
+			} else {
+				var newVal = oldValue + 1;
+			}
+			spinner.find("input").val(newVal);
+			spinner.find("input").trigger("change");
+		});
+
+		btnDown.click(function() {
+			var oldValue = parseFloat(input.val());
+			if (oldValue <= min) {
+				var newVal = oldValue;
+			} else {
+				var newVal = oldValue - 1;
+			}
+			spinner.find("input").val(newVal);
+			spinner.find("input").trigger("change");
+		});
 	});
 
 
